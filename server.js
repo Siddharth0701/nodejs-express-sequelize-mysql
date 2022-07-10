0b3cdeda08d98d10b9a30997aba52be81c3c5b99
Siddharth <siddharth.singh0701@gmail.com>
2022-07-10 20:11:33 +0530

    added controller and route folders

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express=require("express");
 const cors=require("cors");
+const bodyParser = require("body-parser");
 const app=express();
+
 var corsOption={
     origin :"https://localhost:8081"
 };
 app.use(cors(corsOption));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+//app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+const db=require("./app.models");
+db.sequelize.sync()
+.then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 app.get("/",(req,res)=>{
     res.json({message:"Welcome to express node js application."});
 
